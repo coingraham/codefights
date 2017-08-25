9f6f6f0eec82ac0501a96ee3d3050d09881e1fb1
Coin Graham <cgraham@2ndwatch.com>
2017-08-25 08:48:26 -0400

    allLongestStrings solution

---
+++ python/allLongestStrings/allLongestStrings.py
@@ -0,0 +1,18 @@
+def allLongestStrings(inputArray):
+    longest = 0
+    longest_array = []
+
+    for string in inputArray:
+        if len(string) > longest:
+            longest = len(string)
+            longest_array = [string]
+            continue
+
+        if len(string) == longest:
+            longest_array.append(string)
+
+    return longest_array
+
+
+if __name__ == '__main__':
+    print allLongestStrings(["aba", "aa", "ad", "vcd", "aba"])
