d6a434883baeb76739b3ad3c91b042faa5897f06
Coin Graham <cgraham@2ndwatch.com>
2017-09-10 10:25:54 -0400

    Updates

--- python/absoluteValuesSumMinimization/absoluteValuesSumMinimization.py
+++ python/absoluteValuesSumMinimization/absoluteValuesSumMinimization.py
@@ -47,6 +47,9 @@ def absoluteValuesSumMinimization(a):
 
     return min(success)
 
+def absoluteValuesSumMinimization2(A):
+    # This was the top rated solution.... amazing.
+    return A[(len(A)-1)/2]
+
 if __name__ == '__main__':
-    print absoluteValuesSumMinimization([-1000000, -11, -12, -10, -9, -8, -7, -6, -5, -4, -3,
-                                         -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1000000])
+    print absoluteValuesSumMinimization2([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
