76ca54bfc9a1a7206d3df5435515cfa7dabac516
Coin Graham <cgraham@2ndwatch.com>
2017-10-14 23:03:56 -0400

    New solution for arrayMaxConsecutiveSum

--- python/arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.py
+++ python/arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.py
@@ -35,8 +35,11 @@ Guaranteed constraints:
 The maximal possible sum.
 """
 
-
-def arrayMaxConsecutiveSum(inputArray, k):
+"""
+This is the original code I used to solve for this problem but it runs at O(n^2) which is too slow once you start
+to utilize larger arrays and larger k values.  The re-work runs at O(n) which is very fast in comparison.
+"""
+def arrayMaxConsecutiveSum_old(inputArray, k):
     max = 0
 
     for x in range(0, len(inputArray) - k + 1):
@@ -44,6 +47,26 @@ def arrayMaxConsecutiveSum(inputArray, k):
             max = sum(inputArray[x:x+k])
 
     return max
+
+
+def arrayMaxConsecutiveSum(inputArray, k):
+
+    max_sum = sum(inputArray[0:k])
+    current = sum(inputArray[0:k])
+
+    for x in range(0, len(inputArray) - k):
+        right_add = inputArray[k + x]
+        left_subtract = inputArray[0 + x]
+        new_total = current + right_add - left_subtract
+
+        if new_total > max_sum:
+            max_sum = new_total
+            current = new_total
+        else:
+            current = new_total
+
+    return max_sum
+
 
 if __name__ == '__main__':
     print arrayMaxConsecutiveSum([2, 3, 5, 4, 6], 2)
